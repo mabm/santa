7d27f3a95ac42a1394d149c0c66bf0165994ef0d
Joris Bertomeu <joris.bertomeu@epitech.eu>
2015-01-17 12:14:29 +0100

    Auto Commit

--- Makefile
+++ Makefile
@@ -1,13 +1,3 @@
-##
-## Makefile for  in /home/ades_n/PiscineCPP/santa
-##
-## Made by ades nicolas
-## Login   <ades_n@epitech.net>
-##
-## Started on  Sat Jan 17 10:24:32 2015 ades nicolas
-## Last update Sat Jan 17 10:31:42 2015 ades nicolas
-##
-
 SRC	=	AToy.cpp	\
 		Box.cpp		\
 		GiftPaper.cpp	\
@@ -22,14 +12,14 @@ NAME	=	Santa
 
 CXFLAGS +=	-Wall -Wextra -Werror
 
-CCX	=	clang++
+CXX	=	clang++
 
 RM	=	rm -rf
 
 all	:	$(NAME)
 
 $(NAME)	:	$(OBJ)
-		$(CCX) $(OBJ) -o $(NAME)
+		$(CXX) $(OBJ) -o $(NAME)
 
 clean	:
 		$(RM) $(OBJ)
--- Wrap.hh
+++ Wrap.hh
@@ -5,11 +5,12 @@
 // Login   <jobertomeu@epitech.net>
 // 
 // Started on  Sat Jan 17 00:13:03 2015 Joris Bertomeu
-// Last update Sat Jan 17 00:55:12 2015 Joris Bertomeu
+// Last update Sat Jan 17 11:56:14 2015 Joris Bertomeu
 //
 
 #ifndef		_WRAP_H_
 # define	_WRAP_H_
+# include	<iostream>
 # include	"Object.hh"
 
 class		Wrap
@@ -17,10 +18,10 @@ class		Wrap
 protected:
   Object	*objectIn;
 public:
-  Wrap() {}
-  virtual	~Wrap() {}
-  virtual bool	wrapMeThat(Object toWrap) = 0;
-  virtual Object	*openMe() = 0;
+  Wrap();
+  virtual	~Wrap();
+  virtual bool	wrapMeThat(Object toWrap);
+  virtual Object	*openMe();
   Object	*MyUnitTests(Object **);
 };
 
