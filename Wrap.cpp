fe43629db2ec6512a88fb9e8e3f6d871fe8b25b6
Torre Pierre <torre_p@epitech.eu>
2015-01-17 12:56:22 +0100

    errorsFixing

--- Wrap.cpp
+++ Wrap.cpp
@@ -11,8 +11,6 @@ Object*		Wrap::getObjectIn() const
   return this->objectIn;
 }
 
-Object		*Wrap::MyUnitTests(Object **tab)
-{
 Wrap::Wrap()
 {
   std::cout << "[INFO]\tCreating Wrap !" << std::endl;
@@ -59,10 +57,10 @@ Object		*Wrap::MyUnitTests(Object **tab)
   return (tab[2]);
 }
 
-std::ostream&	operator<<(std::ostream &, Wrap const & elem)
+std::ostream&	operator<<(std::ostream & os, Wrap const & elem)
 {
   if (elem.getObjectIn())
-    std::cout << "[INFO]\tThis Wrap contains something." << std::endl;
+    return (os << "[INFO]\tThis Wrap contains something.\n");
   else
-    std::cout << "[INFO]\tThis Wrap contains nothing." << std::endl;
+    return (os << "[INFO]\tThis Wrap contains nothing.\n");
 }
