707717167f972fd316e99c3da05e516cf3a694a5
ades_n <nicolas.ades@epitech.eu>
2015-01-17 17:56:08 +0100

    toto

---
+++ new/IElf.hh
@@ -0,0 +1,21 @@
+//
+// IElf.hh for  in /home/ades_n/PiscineCPP/santa/new
+//
+// Made by ades nicolas
+// Login   <ades_n@epitech.net>
+//
+// Started on  Sat Jan 17 17:41:09 2015 ades nicolas
+// Last update Sat Jan 17 17:43:51 2015 ades nicolas
+//
+
+#ifndef		IELF_H_
+# define	IEFL_H_
+
+class		IElf
+{
+public:
+  virtual	~IElf() {}
+
+};
+
+#endif /* IELF_H_ */
