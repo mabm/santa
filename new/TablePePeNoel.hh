9c7f9154b4ac0b5a36109c3785b785dd7cf61b6f
barnea_v <barnea_v@epitech.eu>
2015-01-17 15:48:20 +0100

    bool

---
+++ new/TablePePeNoel.hh
@@ -0,0 +1,32 @@
+//
+// TablePePeNoel.hh for TablePePeNoel in /home/barnea_v/rendu/santa
+// 
+// Made by Viveka BARNEAUD
+// Login   <barnea_v@epitech.net>
+// 
+// Started on  Sat Jan 17 11:47:24 2015 Viveka BARNEAUD
+// Last update Sat Jan 17 15:46:36 2015 Viveka BARNEAUD
+//
+
+#ifndef		TABLEPEPENOEL_HH_
+# define	TABLEPEPENOEL_HH_
+
+# include	"ITable.hh"
+
+class		TablePePeNoel : public ITable
+{
+private:
+  Object	*_stack[10];
+public:
+  TablePePeNoel();
+  TablePePeNoel(TablePePeNoel const& other);
+  ~TablePePeNoel();
+  TablePePeNoel	&operator=(TablePePeNoel const& other);
+  bool		putObject(Object *o);
+  Object	*takeObject(int idx);
+  Object	*takeObject();
+  void		Look() const;
+  void		Look(int idx) const;
+};
+
+#endif		/* TABLEPEPENOEL_HH_ */
